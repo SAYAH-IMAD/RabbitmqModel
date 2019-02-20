3abbb0c8fb8818255bd35907ef90056545647045
isayah <sayah.imad@outlook.fr>
2019-02-20 23:01:21 +0100

    adding project

---
+++ queue/receiver/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace receiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionFactory connectionFactory = new ConnectionFactory();
+            
+            connectionFactory.UserName="guest";
+            connectionFactory.Password="guest";
+            connectionFactory.HostName ="localhost";
+            connectionFactory.VirtualHost="/";   
+
+            using (IConnection connection = connectionFactory.CreateConnection())
+            {
+                using (IModel channel = connection.CreateModel())
+                {
+                    int i = 0;
+                    channel.QueueDeclare(queue: "learnrabbit",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine($" message {i} Received {message}");
+                        i++;
+                    };
+                    channel.BasicConsume(queue: "learnrabbit",
+                                        autoAck: false,
+                                        consumer: consumer);
+                                        
+                    Console.WriteLine("project receiver");
+
+                    Console.ReadKey();
+
+                }
+                
+            }
+        }
+    }
+}
---
+++ queue/sender/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace sender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionFactory connectionFactory = new ConnectionFactory();
+            
+            connectionFactory.UserName="guest";
+            connectionFactory.Password="guest";
+            connectionFactory.HostName ="localhost";
+            connectionFactory.VirtualHost="/";   
+
+            using (IConnection connection = connectionFactory.CreateConnection())
+            {
+                using (IModel channel = connection.CreateModel())
+                {
+
+                       channel.QueueDeclare(queue: "learnrabbit",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+                    string message = "Hello World!";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    
+
+                    for (int i = 0; i < 1000000; i++)
+                    {
+                        channel.BasicPublish(exchange: "",
+                                        routingKey: "learnrabbit",
+                                        basicProperties: null,
+                                        body: body);
+                                        
+                        Console.WriteLine($" message {i} Sent {message}");
+                    }
+                  
+                }
+            }
+          
+              
+            Console.WriteLine("project sender");
+        }
+    }
+}
